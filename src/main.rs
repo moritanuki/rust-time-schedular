633585fe5eabe48dbf25e7ba6d394bb46457c8b7
moritanuki <ilovfam12@gmail.com>
2022-12-25 14:55:25 +0900

    適当なAPIをtime schdulerで叩く

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use job_scheduler::{JobScheduler, Job};
 use std::time::Duration;
+use reqwest::Client;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[tokio::main]
+async fn get() -> Result<()> {
+    let client = Client::new();
+    let url = "https://zipcloud.ibsnet.co.jp/api/search";
+    let response = client
+        .get(url)
+        .query(&[("zipcode", "1000002")])
+        .send()
+        .await?;
+    
+    let body = response.text().await?;
+    println!("{}", body);
+    Ok(())
+}
 
 fn main() {
     let mut sched = JobScheduler::new();
 
-    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
-        println!("hello");
+    sched.add(Job::new("0 * * * * *".parse().unwrap(), || {
+        let _result = get();
     }));
 
     loop {
@@ -13,4 +31,4 @@ fn main() {
 
         std::thread::sleep(Duration::from_millis(500));
     }
-}
+  }
